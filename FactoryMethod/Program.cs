12193454f6330f4e5b9cf7e44bea261a81441f57
abdulhajiyev <abdulhajiyev@gmail.com>
2021-10-04 09:20:12 -0700

    FactoryMethod Demo

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -6,7 +6,49 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Logistics logistics = new SeaLogistics();
+            ITransport transport = logistics.CreateTransport();
+            transport.Deliver();
         }
+    }
+
+    public interface ITransport
+    {
+        public void Deliver();
+    }
+
+    class Truck : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Land");
+        }
+    }
+
+    class Ship : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Sea");
+        }
+    }
+
+    public abstract class Logistics
+    {
+        public void Delivery()
+        {
+            Console.WriteLine("Delivery");
+        }
+        public abstract ITransport CreateTransport();
+    }
+
+    class RoadLogistics : Logistics
+    {
+        public override ITransport CreateTransport() => new Truck();
+    }
+
+    class SeaLogistics : Logistics
+    {
+        public override ITransport CreateTransport() => new Ship();
     }
 }
